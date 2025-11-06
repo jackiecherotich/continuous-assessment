3c199a50d91bcd11a53cdfb3fabc271dde536684
jackiecherotich <jackie.cherotich@gmail.com>
2025-11-06 23:30:08 +0000

    Terraform scripts

---
+++ instance.tf
@@ -0,0 +1,35 @@
+
+# Custom VPC Provisioning #
+resource "aws_vpc" "ca_vpc" {
+  cidr_block       =  var.vpc_cidr
+  tags = {
+    Name = "custom-vpc"
+  }
+}
+# EC2 Instance under Custom VPC
+
+resource "aws_instance" "server_instance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = aws_key_pair.ssh_key.key_name
+  #count                  = var.instance_count
+
+  tags = { Name = "webserver-instance-1" }
+}
+
+# Allocate Elastic IP and attach to each EC2 instance
+resource "aws_eip" "server_eip" {
+  instance = aws_instance.server_instance.id
+       
+
+  tags = { Name = "webserver-elastic-ip-1" }
+}
+
+# Generate Ansible inventory for 
+resource "null_resource" "generate_inventory" {
+  provisioner "local-exec" {
+    command = "printf '[server_instance]\\n%s ansible_user=ubuntu ansible_ssh_private_key_file=%s\\n' ${aws_eip.server_eip.public_ip} /home/precious/ssh-keys/ec2-instance-key.pem > ${path.module}/inventory.ini"
+  }
+}
